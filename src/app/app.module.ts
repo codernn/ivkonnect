611d49e45e54fe88dfdeae0dde7b021ff614534d
codernn <schakradhar129@gmail.com>
2020-05-23 17:54:00 +0530

    cards updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { SectorsComponent } from './components/sectors/sectors.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    SectorsComponent,
   ],
   imports: [
     BrowserModule,
